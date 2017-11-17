979c44d6b294ba6cb208f263f0a133b44a699bff
Neo Lee <neo.lee@gmail.com>
2017-11-17 18:00:33 +0800

    Update assets

--- clojure/project.clj
+++ clojure/project.clj
@@ -3,13 +3,12 @@
   :url "https://github.com/neolee/twat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [org.apache.commons/commons-math3 "3.6.1"]
-                 [org.clojure/clojure "1.8.0"]
-                 [criterium "0.4.4"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/data.csv "0.1.3"]
+                 [org.clojure/data.csv "0.1.4"]
                  ]
-  :plugins [[lein-clean-m2 "0.1.2"]]
-  :jvm-opts ["-Xmx1g"])
+  :resource-paths ["resources"])
